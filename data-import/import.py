ad0f0839bc19a338344de1fed69f00e7effbce40
Sylvain Houdusse <syh@slyweb.org>
2016-06-15 00:26:18 +0200

    Import

--- data-import/import.py
+++ data-import/import.py
@@ -11,15 +11,19 @@ def main():
 	    data = json.load(data_file)
 
 	cnx = mysql.connector.connect(user='pasteque', password='pasteque',
-	                              host='pasteque-db',
+	                              host='pasteque.escale-biere.com',
 	                              database='pasteque')
 
 	curA = cnx.cursor(buffered=True)
 	query = ("SELECT REFERENCE FROM PRODUCTS")
 	curA.execute(query)
 	stock = curA.fetchall()
+	curB = cnx.cursor(buffered=True)
+	query = ("SELECT REFERENCE FROM CATEGORIES")
+	curB.execute(query)
+	categories = curB.fetchall()
 
-	from pprint import pprint; import ipdb; ipdb.set_trace()
+
 
 	for line in data:
 		if 'achetLe' in line and 'type' in line and line['type'] != u'coffret' and line['type'] != u'Coffret':
@@ -30,14 +34,27 @@ def main():
 			print "  Prix achat: %s" % line['pu']
 			print "  Quantité: %i" % (int(line['nbreCartons']) * int(line['parCarton']))
 			print "  Code: %s" % line['codification']
-			if (line['codification'],) in stock
-				add_product()
+			sp = line['codification'].split('-')
+			cat_ref = sp[0]
+			category_id = get_category_id_from_reference(cnx, cat_ref)
+			# if (line['codification'],) in stock:
+			# 	add_product(cnx)
 			save_in_db(cnx, line['brasserie'], line['nomDeLaBire'], line['pu'], int(line['nbreCartons']) * int(line['parCarton']), line['codification'])
+			import ipdb; ipdb.set_trace()
 
 	cnx.close()
 
 def save_in_db(cnx, brasseur, type, price, quantity, codification):
 	return True
+
+def add_product(cnx):
+	return True
+
+def get_category_id_from_reference(cnx, reference):
+	cur = cnx.cursor(buffered=True)
+	query = ("SELECT ID FROM CATEGORIES WHERE REFERENCE='%s'" % reference)
+	cur.execute(query)
+	return cur.fetchone()[0]
 
 if __name__ == '__main__':
 	main()
