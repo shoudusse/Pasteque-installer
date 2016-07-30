15f81297c764fd53106f21d5c332a19c090a4079
Sylvain Houdusse <syh@slyweb.org>
2016-07-30 13:56:36 +0200

    Add replication user

--- ansible/roles/mariadb/templates/init-mysql.sql
+++ ansible/roles/mariadb/templates/init-mysql.sql
@@ -1,4 +1,6 @@
 CREATE USER 'pasteque'@'localhost' IDENTIFIED BY 'pasteque';
+CREATE USER 'replication'@'10.8.0.1' IDENTIFIED BY 'replication';
+GRANT REPLICATION SLAVE ON *.* TO 'replication'@'10.8.0.1';
 CREATE USER 'pasteque'@'{{ hostvars['pasteque-web']['ansible_eth0']['ipv4']['address'] }}' IDENTIFIED BY 'pasteque';
 GRANT ALL PRIVILEGES ON pasteque . * TO 'pasteque'@'localhost';
 GRANT ALL PRIVILEGES ON pasteque . * TO 'pasteque'@'{{ hostvars['pasteque-web']['ansible_eth0']['ipv4']['address'] }}';
